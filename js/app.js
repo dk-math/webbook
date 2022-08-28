742e87b6302f2d4fafba12f49936e2c6e18d4ff0
dk-math <dk.gianluigi.1978@gmail.com>
2022-08-28 21:57:25 +0900

    tab active modified

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const tabs = document.getElementsByClassName('tab');
+const tabs = document.getElementsByClassName('tab_item');
 for(let i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener('click', tabSwitch, false);
 }
@@ -6,4 +6,8 @@ for(let i = 0; i < tabs.length; i++) {
 function tabSwitch(){
     document.getElementsByClassName("active")[0].classList.remove("active");
     this.classList.add("active");
+    document.getElementsByClassName("is-show")[0].classList.remove('is-show');
+    const arrayTabs = Array.prototype.slice.call(tabs);
+    const index = arrayTabs.indexOf(this);
+    document.getElementsByClassName("tab_content")[index].classList.add('is-show');
 };
